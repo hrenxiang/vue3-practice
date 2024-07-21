2aab131e3456e707ffe179305f2cd92a7ac05a25
huangrx <2295701930@qq.com>
2024-07-21 17:48:15 +0800

    feat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import WatchRefBasic from "@/components/watch/watchRefBasic.vue";
+import WatchRefObject from "@/components/watch/watchRefObject.vue";
+import WatchReactiveObject from "@/components/watch/watchReactiveObject.vue";
+import WatchRefOrReactiveObjectField from "@/components/watch/watchRefOrReactiveObjectField.vue";
+import WatchRefOrReactiveObjectFields from "@/components/watch/watchRefOrReactiveObjectFields.vue";
+import WatchEffectDemo from "@/components/watch/watchEffectDemo.vue";
+</script>
+
+<template>
+  <!--  <WatchRefBasic/>-->
+  <!--  <WatchRefObject/>-->
+  <!--  <WatchReactiveObject/>-->
+  <!--  <WatchRefOrReactiveObjectField/>-->
+  <!--  <WatchRefOrReactiveObjectFields/>-->
+  <WatchEffectDemo/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/watch/watchRefBasic.vue
@@ -0,0 +1,32 @@
+<script setup name="WatchRefBasic">
+import {ref, watch} from 'vue';
+// 数据
+let sum = ref(0)
+
+// 方法
+function changeSum() {
+  sum.value += 1
+}
+
+// 监视，情况一：监视【ref】定义的【基本类型】数据
+const stopWatch = watch(sum, (newValue, oldValue) => {
+  console.log('sum变化了', newValue, oldValue)
+  if (newValue >= 10) {
+    stopWatch()
+  }
+})
+</script>
+
+<template>
+  <div class="person">
+    <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+
+    <h2>当前求和为：{{ sum }}</h2>
+
+    <button @click="changeSum">点我sum+1</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
